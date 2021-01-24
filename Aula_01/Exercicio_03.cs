482cf73d36ba51087eaeb9d86b282423f06828f0
magicbits_ <73851455+magic-bits@users.noreply.github.com>
2021-01-24 14:48:29 -0300

    Create Exercicio_03.cs

---
+++ Aula_01/Exercicio_03.cs
@@ -0,0 +1,14 @@
+string s1 = "Estou";
+string s2 = "achando";
+string s3 = "este";
+string s4 = "exercicio";
+string s5 = "muito";
+string s6 = "legal!!!";
+
+//Console.Write (s1 + "\n" + s2 + "\n" + s3 + "\n" + s4 + "\n" + s5 + "\n" + s6);
+Console.WriteLine (s1);
+Console.WriteLine (s2);
+Console.WriteLine (s3);
+Console.WriteLine (s4);
+Console.WriteLine (s5);
+Console.WriteLine (s6);
