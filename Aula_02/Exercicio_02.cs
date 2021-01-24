bf0de34ecf03dea97fe78de9279fd1a6143949fc
magicbits_ <73851455+magic-bits@users.noreply.github.com>
2021-01-24 15:22:47 -0300

    Create Exercicio_02.cs

---
+++ Aula_02/Exercicio_02.cs
@@ -0,0 +1,28 @@
+```csharp
+int LerEntrada()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int Quadrado(int numero)
+{
+    return numero*numero;
+}
+
+bool Pitagoras (int x, int y, int z)
+{
+    return ((Quadrado(x)+Quadrado(y)) == Quadrado(z));
+}
+
+bool EscreverSaida()
+{
+    int a = LerEntrada();
+    int b = LerEntrada();
+    int c = LerEntrada();
+    return Pitagoras(a, b, c);
+}
+
+bool s = EscreverSaida();
+Console.WriteLine(s);
+```
