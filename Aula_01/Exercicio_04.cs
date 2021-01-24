3e438d80acfec68a95092ea4afbc102390255879
magicbits_ <73851455+magic-bits@users.noreply.github.com>
2021-01-24 14:49:10 -0300

    Create Exercicio_04.cs

---
+++ Aula_01/Exercicio_04.cs
@@ -0,0 +1,8 @@
+int cont = 1;
+Console.WriteLine ("Esta é a linha " + cont);
+cont = cont + 1;
+Console.WriteLine ("Esta é a linha " + cont);
+cont += 1;
+Console.WriteLine ("Esta é a linha " + cont);
+cont++;
+Console.WriteLine ("Esta é a linha " + cont);
